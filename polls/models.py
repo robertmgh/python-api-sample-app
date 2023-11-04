6acf77af0b60c3a50c63ea0c5564a0aaaa478a6e
Robert Michnowski <robert.michnowski@onet.eu>
2023-11-04 20:28:34 +0100

    Init model User

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
+    admin = models.BooleanField(default=False)
+    active_from = models.DateTimeField()
+
